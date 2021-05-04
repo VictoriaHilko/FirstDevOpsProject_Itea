d72434cf26e92e18bc2a19d5a9af679a07eacb94
VictoriaHilko <47423052+VictoriaHilko@users.noreply.github.com>
2021-05-04 15:41:44 +0300

    Update server1.sh

--- terraform/server1.sh
+++ terraform/server1.sh
@@ -13,4 +13,5 @@ apt-get install docker-ce docker-ce-cli containerd.io
 curl -L "https://github.com/docker/compose/releases/download/1.29.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 git clone https://github.com/VictoriaHilko/FirstDevOpsProject_Itea.git
+cd FirstDevOpsProject_Itea
 docker-compose up -d
